8c5890228d22ad6a0abb485fd05f4c204fa9ba49
Sawyer LIN <sawyer.lin@gmail.com>
2014-02-18 22:43:09 +0100

    Add Template

---
+++ Sel/app/app.js
@@ -0,0 +1,16 @@
+﻿var selApp = angular.module('selApp', [
+    'ngRoute',
+    'user',
+    'advert'
+    ]);
+
+selApp.config([
+    '$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl:'partials/home.html',
+                controller:'AdvertListCtrl'
+            });
+    }
+]);
